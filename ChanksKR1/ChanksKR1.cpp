2e10821e822702ea687f1f3cb589386ca41c7fb4
Александр Клейменов <gaganertime88@gmail.com>
2021-12-07 23:08:29 +0300

    pid

--- ChanksKR1/ChanksKR1.cpp
+++ ChanksKR1/ChanksKR1.cpp
@@ -3,12 +3,39 @@
 #include <vector>
 #include <ctime>
 #include <cmath>
-#define _USE_MATH_DEFINES
 using namespace std;
 
 const double pi = 3.14159265358979323846;
 
-//метод для перевода из строкового в целочисленный тип данных
+
+int         lastPID                   = 0;
+int         angle                     = 0; 
+
+float       lastErr                   = 0;
+float       lastLastErr               = 0;
+float       lenght                    = 0;
+
+const int   numberOfChanks            = 16;
+const int   numberOfInternalChanks    = 4;
+const int   numberOfCornerChanks      = 4;
+const int   numberOfDirectionChanks   = 8;
+
+const int   firstQuarter[2]           = {1,1};
+const int   secondQuarter[2]          = {-1,1};
+const int   thirdQuarter[2]           = {-1,-1};
+const int   fourthQuarter[2]          = {1,-1};
+
+//структура каждой точки с информацией о ней же 
+//(IDK COMPLETE)
+struct point
+{
+    float  spacing;
+    int    coordinates[2];
+    int    index;
+};
+
+//метод для перевода из строкового в целочисленный тип данных 
+//(COMPLETE)
 string to_string(int n[])
 {
     ostringstream ss;
@@ -16,13 +43,18 @@ string to_string(int n[])
     return ss.str();
 }
 
-//Регулятор
+//Регулятор 
+//(COMPLETE, WAIT TESTING)
 int PID(int spdL, int spdR, float err, float Kp, float Ki, float Kd, vector<float> sensorsValue, int timerValue) 
 {
-    return 0;
+    int PID      = lastPID + Kp * Ki * err + Kp * (err - lastErr) + ((Kp * Kd) / timerValue) * (err - 2 * lastErr + lastLastErr);
+    lastPID      = PID;
+    lastLastErr  = lastErr;
+    lastErr      = err;
+    return PID;
 }
 
-//Координаты конца вектора
+//Координаты конца вектора (COMPLETE, WAIT TESTING)
 vector<float> coordEndVector(float length, int angleAndIndex) 
 {
     vector<float> coord;
@@ -33,21 +65,15 @@ vector<float> coordEndVector(float length, int angleAndIndex)
     return coord;//yVector; 
 }
 
-//расчёт вхождения точек в чанки
+//расчёт вхождения точек в чанки 
+//(UNCOMPLETE)
 bool comparison() 
 {
     return 0;
 }
 
-//структура каждой точки с информацией о ней же
-struct point 
-{
-    float spacing;
-    int coordinates[2];
-    int index;
-};
-
-//структура одного чанка
+//структура одного чанка 
+//(UNCOMPLETE)
 class Chank {
 private:
     static const unsigned int Vertex = 4;
@@ -57,17 +83,17 @@ public:
     Chank() {
     }
     Chank(int x1, int y1, int x2, int y2, int x3, int y3, int x4, int y4) {
-        vertexes[0][0] = x1; //{x1, y1}
-        vertexes[0][1] = y1; //{x1, y1}
+        vertexes[0][0] = x1; //x1,
+        vertexes[0][1] = y1; //y1
 
-        vertexes[1][0] = x2; //{x2, y2}
-        vertexes[1][1] = y2; //{x2, y2}
+        vertexes[1][0] = x2; //x2
+        vertexes[1][1] = y2; //y2
 
-        vertexes[2][0] = x3; //{x3, y3}
-        vertexes[2][1] = y3; //{x3, y3}
+        vertexes[2][0] = x3; //x3
+        vertexes[2][1] = y3; //y3
 
-        vertexes[3][0] = x4; //{x4, y4}
-        vertexes[3][1] = y4; //{x4, y4}
+        vertexes[3][0] = x4; //x4
+        vertexes[3][1] = y4; //y4
     }
 
     int vertexes[Vertex][Coordinates]; //координаты вершины каждого квадрата(чанка)
@@ -97,33 +123,15 @@ public:
 };
 
 
-
-const int numberOfChanks = 16;
-const int numberOfInternalChanks = 4;
-const int numberOfCornerChanks = 4;
-const int numberOfDirectionChanks = 8;
-
 Chank internalArrayMap[numberOfInternalChanks];//массив внешних чанков (0, 1, 2, 3 согласно рисунку на флипчарте)
 Chank cornerArrayMap[numberOfCornerChanks];//массив чанков, которые операются на углы (6, 9, 12, 15 согласно рисунку на флипчарте)
 Chank directionArrayMap[numberOfDirectionChanks];//массив чанков, которые операются на стороны(4, 5, 7, 8, 10, 11, 13, 14 согласно рисунку на флипчарте)
-
-//константы для определенния коэффициентов координат точек массивов
-const int firstQuarter[2] = { 1, 1 };
-const int secondQuarter[2] = { -1, 1 };
-const int thirdQuarter[2] = { -1, -1 };
-const int fourthQuarter[2] = { 1, -1 };
-float lenght; 
-int angle;
 
 int main()
 {
     //ros::ok()
     while (true) {
         unsigned int startTime = clock();
-        for (int i = 0; i < numberOfChanks; i++)
-        {
-
-        }
         cin >> lenght;
         cin >> angle;
         cout << coordEndVector(lenght, angle)[0] << "  ";
@@ -132,5 +140,4 @@ int main()
         float timer = endTime - startTime;
         cout << timer << endl;
     }
-
 }
